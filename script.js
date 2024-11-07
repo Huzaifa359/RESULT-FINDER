f494857804c9054aea5bbe94a739c8cad89de829
Huzaifa359 <huzaifaahmed56836@gmail.com>
2024-11-07 23:50:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var a = prompt("enter your roll no only digit")
+switch (a) {
+    case "271238":
+        document.write("Name : Muhammad umair khan&nbsp" + "<br><br><br>" + "percentage : 93% <br><br><br>status : pass ");
+        break;
+    case "270914":
+        document.write("Name : Muhammad junaid khan &nbsp" + "<br><br><br>" + "percentage : 90% <br><br><br>status : pass ");
+        break;
+    case "254515":
+        document.write("Name : Ibrahim &nbsp" + "<br><br><br>" + "percentage : 90% <br><br><br>status : passe ");
+        break;
+    case "265574":
+        document.write("Name :Areeb Khan Anwar &nbsp" + "<br><br><br>" + "percentage : 87% <br><br><br>status : pass");
+        break;
+    case "254684":
+        document.write("Name :Asad ullah &nbsp" + "<br><br><br>" + "percentage : 87% <br><br><br>status : pass");
+        break;
+    case "265887":
+        document.write("Name :Azeem Shahid &nbsp" + "<br><br><br>" + "percentage : 83% <br><br><br>status : pass");
+        break;
+    case "255284":
+        document.write("Name :Ahmed Ali&nbsp" + "<br><br><br>" + "percentage : 83% <br><br><br>status : pass");
+        break;
+    case "265574":
+        document.write("Name :Abdul moiz &nbsp" + "<br><br><br>" + "percentage : 83% <br><br><br>status : pass");
+        break;
+    case "260679":
+        document.write("Name :abdul muiz " + "<br><br><br>" + "percentage : 83% <br><br><br>status : pass");
+        break;
+    case "252944":
+        document.write("Name :Muhammad faizan " + "<br><br><br>" + "percentage : 80% <br><br><br>status : pass ");
+        break;
+    case "257764":
+        document.write("Name : M reahan rashid" + "<br><br><br>" + "percentage : 80% <br><br><br>status : pass");
+        break;
+    case "268417":
+        document.write("Name: Ali slahuddin" + "<br><br><br>" + "percentage : 77% <br><br><br>status : pass");
+        break;
+    case "269662":
+        document.write("Name : Huzaifa ahmed" + "<br><br><br>" + "percentage : 70% <br><br><br>status : pass");
+        break;
+        default:
+            document.write("<h1>Result Not Found </h1>")
+}
+
